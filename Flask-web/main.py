b94a7f80e3baf174fbb1870eb478cb8ada9eaaf5
landehuxi <925127008@qq.com>
2020-12-16 18:09:59 +0800

    sijie

--- Flask-web/main.py
+++ Flask-web/main.py
@@ -1,3 +1,8 @@
-import flask as Flask
-
-print('hello world')
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello Liangruanruan , this my first wed file!'
+if __name__ == '__main__':
+    app.debug = True # 设置调试模式，生产模式的时候要关掉debug
+    app.run()
